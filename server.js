aa117c8de63d0fe3ba7b70df858068cdd9b9ffbf
Alex Yong <alexjyong@gmail.com>
2023-05-31 18:11:18 +0000

    redirect update

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const app = express();
 // Allow the app to use JSON
 app.use(express.json());
 
-// Serve the index.html file when root ('/') is accessed
+// Redirect the root path ('/') to '/Auditor/Mfg-Homes-Counter/'
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.redirect('/Auditor/Mfg-Homes-Counter/');
 });
 
-// Define the route for '/Auditor/Mfg-Homes-Counter/'
+// Serve the index.html file when '/Auditor/Mfg-Homes-Counter/' is accessed
 app.get('/Auditor/Mfg-Homes-Counter/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
